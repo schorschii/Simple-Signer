2986c791b7a24fe603768970cbbbad9874bc4690
Georg Sieber <it@georg-sieber.de>
2022-03-11 19:24:11 +0100

    add certify menu item (and shortcut)

--- lang/de.ts
+++ lang/de.ts
@@ -14,67 +14,67 @@
         <translation>&amp;Signieren</translation>
     </message>
     <message>
-        <location filename="../simple-signer.py" line="97"/>
+        <location filename="../simple-signer.py" line="101"/>
         <source>Choose &amp;PDF File...</source>
         <translation>&amp;PDF-Datei auswählen...</translation>
     </message>
     <message>
-        <location filename="../simple-signer.py" line="101"/>
-        <source>Choose &amp;Certificate File...</source>
-        <translation>&amp;Zertifikats-Datei auswählen...</translation>
+        <location filename="../simple-signer.py" line="105"/>
+        <source>Choose C&amp;ertificate File...</source>
+        <translation>Z&amp;ertifikats-Datei auswählen...</translation>
     </message>
     <message>
-        <location filename="../simple-signer.py" line="106"/>
+        <location filename="../simple-signer.py" line="110"/>
         <source>&amp;Quit</source>
         <translation>&amp;Beenden</translation>
     </message>
     <message>
-        <location filename="../simple-signer.py" line="112"/>
+        <location filename="../simple-signer.py" line="116"/>
         <source>&amp;Help</source>
         <translation>&amp;Hilfe</translation>
     </message>
     <message>
-        <location filename="../simple-signer.py" line="114"/>
+        <location filename="../simple-signer.py" line="118"/>
         <source>&amp;About</source>
         <translation>&amp;Informationen</translation>
     </message>
     <message>
-        <location filename="../simple-signer.py" line="122"/>
+        <location filename="../simple-signer.py" line="203"/>
         <source>PDF File</source>
         <translation>PDF-Datei</translation>
     </message>
     <message>
-        <location filename="../simple-signer.py" line="134"/>
+        <location filename="../simple-signer.py" line="138"/>
         <source>Choose...</source>
         <translation>Auswählen...</translation>
     </message>
     <message>
-        <location filename="../simple-signer.py" line="130"/>
+        <location filename="../simple-signer.py" line="207"/>
         <source>Certificate File</source>
         <translation>Zertifikats-Datei</translation>
     </message>
     <message>
-        <location filename="../simple-signer.py" line="138"/>
+        <location filename="../simple-signer.py" line="142"/>
         <source>Certificate Password</source>
         <translation>Zertifikats-Passwort</translation>
     </message>
     <message>
-        <location filename="../simple-signer.py" line="150"/>
+        <location filename="../simple-signer.py" line="154"/>
         <source>Sign</source>
         <translation>Signieren</translation>
     </message>
     <message>
-        <location filename="../simple-signer.py" line="157"/>
+        <location filename="../simple-signer.py" line="161"/>
         <source>Certify</source>
         <translation>Zertifizieren</translation>
     </message>
     <message>
-        <location filename="../simple-signer.py" line="278"/>
+        <location filename="../simple-signer.py" line="282"/>
         <source>Open Directory</source>
         <translation>Verzeichnis öffnen</translation>
     </message>
     <message>
-        <location filename="../simple-signer.py" line="280"/>
+        <location filename="../simple-signer.py" line="284"/>
         <source>Open Signed PDF</source>
         <translation>Signiertes PDF öffnen</translation>
     </message>
@@ -98,6 +98,11 @@ Certifiy will place your signature on the document and lock it after that.</sour
 
 Beim Signieren können mehrere Benutzer ihre digitale Signatur auf einem Dokument platzieren.
 Beim Zertifizieren wird Ihre Unterschrift auf dem Dokument platziert und danach gesperrt.</translation>
+    </message>
+    <message>
+        <location filename="../simple-signer.py" line="96"/>
+        <source>&amp;Certify</source>
+        <translation>&amp;Zertifizieren</translation>
     </message>
 </context>
 </TS>
