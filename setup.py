8ae40e7aadd6a9b3fd0af3165f7201fe6deb599e
Georg Sieber <it@georg-sieber.de>
2023-12-25 22:43:53 +0100

    new debian packaging with dh_virtualenv

---
+++ setup.py
@@ -0,0 +1,34 @@
+from distutils.command.clean import clean
+from distutils import log
+from setuptools import setup
+
+setup(
+      name='simple_signer',
+      version='1.5.0',
+      description='Sign and certify PDF files on Linux with optional visual stamp using a .p12/.pfx certificate',
+      install_requires=[i.strip() for i in open('requirements.txt').readlines()],
+      license='GPL-3.0',
+      author='Georg Sieber',
+      keywords='python3 pdf sign certify certificate stamp',
+      url='https://github.com/schorschii/simple-signer',
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: End Users/Desktop',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX :: Linux',
+            'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+      ],
+      packages=['simple_signer'],
+      package_data={'simple_signer': ['lang/*.qm']},
+      entry_points = {
+            'gui_scripts': [
+                  'simple-signer = simple_signer.simple_signer:main',
+            ],
+      },
+      platforms=['all'],
+      #install_requires=[],
+      #test_suite='tests',
+)
--- start-venv.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/share/simple-signer/venv/bin/python3 /usr/share/simple-signer/simple-signer.py
