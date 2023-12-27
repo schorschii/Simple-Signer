d0b2b374eed6065fe207a2c8ac6a01c9378efc75
Georg Sieber <it@georg-sieber.de>
2023-12-27 15:36:39 +0100

    a new venv .deb packaging approach, python version generic

--- installer/debian/build.sh
+++ installer/debian/build.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 set -e
 
-# build the .deb package
+# build .deb package
 
-LANG=C dpkg-buildpackage --no-sign --build=binary
+# check root permissions
+if [ "$EUID" -ne 0 ]
+    then echo "Please run this script as root!"
+    exit
+fi
+
+# cd to working dir
+cd "$(dirname "$0")"
+
+# compile language files
+make -C ../..
+
+# empty / create necessary directories
+if [ -d "simple-signer/usr" ]; then
+    rm -r simple-signer/usr
+fi
+
+# copy files in place
+install -D -m 644 ../../assets/simple-signer.desktop      -t simple-signer/usr/share/applications
+install -D -m 644 ../../assets/simple-signer.nemo_action  -t simple-signer/usr/share/nemo/actions
+install -D -m 644 ../../lang/*.qm                         -t simple-signer/usr/share/simple-signer/lang
+install -D -m 644 ../../simple_signer/*.py                -t simple-signer/usr/share/simple-signer/simple_signer
+install -D -m 644 ../../requirements.txt                  -t simple-signer/usr/share/simple-signer
+install -D -m 644 ../../setup.py                          -t simple-signer/usr/share/simple-signer
+install -D -m 644 ../../README.md                         -t simple-signer/usr/share/simple-signer
+
+mkdir -p simple-signer/usr/bin
+ln -sf   /usr/share/simple-signer/venv/bin/simple-signer     simple-signer/usr/bin/simple-signer
+
+# build deb
+dpkg-deb -Zxz --build simple-signer
+
+echo "Build finished"
