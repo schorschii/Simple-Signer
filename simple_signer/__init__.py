969081402579048379829c2b8e45a1c068ccc0f3
Georg Sieber <it@georg-sieber.de>
2024-02-27 13:38:51 +0100

    use endesive 2.17.1 with fixed stamp text and bump version

--- simple_signer/__init__.py
+++ simple_signer/__init__.py
@@ -1,7 +1,7 @@
 __title__   = 'Simple Signer'
 __author__  = 'Georg Sieber'
 __license__ = 'GPL-3.0'
-__version__ = '1.5.3'
+__version__ = '1.5.4'
 __website__ = 'https://github.com/schorschii/Simple-Signer'
 
 __all__ = [__author__, __license__, __version__]
