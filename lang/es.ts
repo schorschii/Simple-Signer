ec511e47524a14f15d843a6a82de8d6f7c722104
SirLouen <sir.louen@gmail.com>
2025-01-06 18:11:27 +0100

    Spanish Translation (#26)

---
+++ lang/es.ts
@@ -0,0 +1,172 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES" sourcelanguage="en_US">
+<context>
+    <name>SimpleSigner</name>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="81"/>
+        <source>Simple-Signer allows you to to sign PDFs using a simple user interface.</source>
+        <translation>Simple-Signer te permite firmar PDFs utilizando una interfaz muy sencilla.</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="402"/>
+        <source>Signing allows multiple users to place their digital signature on a document.</source>
+        <translation>Firmar permite a múltiples usuarios colocar una firma digital en un documento.</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="409"/>
+        <source>Certify will place your signature on the document and lock it after that.</source>
+        <translation>Certificar colocará su firma en el documento y lo bloqueará a continuación.</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="96"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="132"/>
+        <source>PDF is empty!</source>
+        <translation>¡El PDF está vacío!</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="140"/>
+        <source>Please select the rectangular area where you stamp should be placed by clicking and holding the left mouse button.</source>
+        <translation>Por favor, selecciona el área rectangular donde su sello deba ser colocado, clicando y sosteniendo el botón izquierdo del ratón.</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="145"/>
+        <source>Page:</source>
+        <translation>Página:</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="160"/>
+        <source>Done</source>
+        <translation>Hecho</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="171"/>
+        <source>Place Stamp</source>
+        <translation>Colocar Sello</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="242"/>
+        <source>&amp;File</source>
+        <translation>&amp;Archivo</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="244"/>
+        <source>&amp;Sign</source>
+        <translation>&amp;Firmar</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="249"/>
+        <source>&amp;Certify</source>
+        <translation>&amp;Certificar</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="256"/>
+        <source>Choose &amp;PDF File...</source>
+        <translation>Seleccione el &amp;Fichero PDF...</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="268"/>
+        <source>Choose C&amp;ertificate File...</source>
+        <translation>Seleccione el &amp;Fichero de Certificación...</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="275"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Salir</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="281"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Ayuda</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="283"/>
+        <source>&amp;About</source>
+        <translation>&amp;Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="487"/>
+        <source>PDF File(s)</source>
+        <translation>Fichero PDF</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="381"/>
+        <source>Choose...</source>
+        <translation>Seleccionar...</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="499"/>
+        <source>Certificate File</source>
+        <translation>Fichero de Certificación</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="364"/>
+        <source>Certificate Password</source>
+        <translation>Contraseña del Certificado</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="377"/>
+        <source>Draw Stamp</source>
+        <translation>Dibujar Sello</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="380"/>
+        <source>(Optional Stamp Image or Configuration File)</source>
+        <translation>(Imagen de Sello Opcional o Fichero de Configuración)</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="401"/>
+        <source>Sign</source>
+        <translation>Firmar</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="408"/>
+        <source>Certify</source>
+        <translation>Certificar</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="503"/>
+        <source>Stamp Image File</source>
+        <translation>Fichero de Imagen del Sello</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="567"/>
+        <source>Certificate Warning</source>
+        <translation>Alerta de Certificado</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="568"/>
+        <source>Your certificate expired on %s. Continue?</source>
+        <translation>Su certificado expiró el %s. ¿Continuar?</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="578"/>
+        <source>Save Filename for Signed PDF</source>
+        <translation>Nombre de Fichero para el PDF Firmado</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="673"/>
+        <source>Successfully saved as »%s«.</source>
+        <translation>Guardado satisfactoriamente como  »%s«.</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="675"/>
+        <source>Open Directory</source>
+        <translation>Abrir Directorio</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="677"/>
+        <source>Open Signed PDF</source>
+        <translation>Abrir PDF Firmado</translation>
+    </message>
+    <message>
+        <location filename="../simple_signer/simple_signer.py" line="32"/>
+        <source>Drag &amp; drop a file here...</source>
+        <translation>Arrastre y Suelte un fichero aquí...</translation>
+    </message>
+</context>
+</TS>
