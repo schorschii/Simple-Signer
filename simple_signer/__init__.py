8eeaca246925f8966ff565da924dfa3dff28bf00
Georg Sieber <it@georg-sieber.de>
2025-04-23 14:54:43 +0200

    bump version

--- simple_signer/__init__.py
+++ simple_signer/__init__.py
@@ -2,7 +2,7 @@ __title__     = 'Simple Signer'
 __author__    = 'Georg Sieber'
 __copyright__ = '© 2021-2025'
 __license__   = 'GPL-3.0'
-__version__   = '1.6.1'
+__version__   = '1.7.0'
 __website__   = 'https://github.com/schorschii/Simple-Signer'
 
 __all__ = [__author__, __license__, __version__]
